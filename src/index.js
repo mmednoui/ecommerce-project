7037c13b0df7300308c3cff3221dd769f9626f30
mohamednoui <gm_noui@esi.dz>
2022-06-06 12:58:40 +0100

    building the header

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import store from "./redux/store";
-import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
