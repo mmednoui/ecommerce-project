f5beecedbde4e9a430e3ef31324bfc396b9d055f
mohamednoui <gm_noui@esi.dz>
2022-06-06 03:42:51 +0100

    create the frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScree";
+import OrderScreen from "./screens/OrderScreen";
+import PlaceOrder from "./screens/PlaceOrder";
+import ProductDetailScreen from "./screens/ProductDetailScreen";
+import ShippingScreen from "./screens/shippingScreen";
+import SignInScreen from "./screens/SignInScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className="container">
+          <Header />
+        </div>
+
+        <main>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/products/:id" component={ProductDetailScreen}></Route>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/signin" component={SignInScreen}></Route>
+          <Route path="/shipping" component={ShippingScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrder}></Route>
+          <Route path="/order/:id" component={OrderScreen}></Route>
+        </main>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
